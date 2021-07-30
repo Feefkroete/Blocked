4a46943a62b7786ef691b9dff4cd2dc7cb0e92aa
Matthi <mhofi04@gmail.com>
2021-07-30 21:59:07 +0200

    Colision kinda verbessert

--- src/de/matthi/blocked/entity/creature/Player.java
+++ src/de/matthi/blocked/entity/creature/Player.java
@@ -10,11 +10,8 @@ import java.awt.image.BufferedImage;
 public class Player extends Creature
 {
     BufferedImage textur;
-    int impulse = 0;
-    boolean o = true;
-    boolean u = true;
-    boolean l = true;
-    boolean r = true;
+    boolean colisionO, colisionU, colisionL, colisionR;
+    int test = 0;
 
     public Player(double posx, double posy, int width, int height, int hp, BufferedImage textur)
     {
@@ -51,69 +48,63 @@ public class Player extends Creature
     @Override
     public void tick()
     {
-        int geschw = 1;
-
-        /*
-        if(impulse > -10)
-        {
-            this.move(0, -1*impulse);
-            impulse--;
+        int geschw = 5;
+        if (KeyInput.sprint) {
+            geschw *= 2;
         }
-         */
-            if (KeyInput.sprint) {
-                geschw *= 1.8;
-            }
-            if (KeyInput.up) {
-                //impulse = 10;
-                if (o) {
+
+        if (KeyInput.up && posy-30 >= 0) {
+            Block blo = Game.getWorld().getBlock((int) (posx - (width/2) + 1) / 60, (int) (posy - (heigth/2) - geschw + 1) / 60);
+            Block bro = Game.getWorld().getBlock((int) (posx + (width/2) - 1) / 60, (int) (posy - (heigth/2) - geschw + 1) / 60);
+            boolean lo = blo == Block.blocks[4] || blo == null;
+            boolean ro = bro == Block.blocks[4] || bro == null;
+                if (lo && ro) {
                     this.move(0, -1 * geschw);
+                    colisionO = false;
                 }
-            }
-            if (KeyInput.down && posy+30 < Game.getWorld().getHeight()*60) {
-                Block lu = Game.getWorld().getBlock((int) (posx - 29) / 60, (int) (posy + 30) / 60);
-                Block ru = Game.getWorld().getBlock((int) (posx + 29) / 60, (int) (posy + 30) / 60);
-                if (lu == Block.blocks[4] || lu == null && ru == Block.blocks[4] || ru == null) {
-                    this.move(0, geschw);
+                else {
+                    colisionO = true;
                 }
-            }
-            if (KeyInput.left && posx-30 >= 0) {
-                Block lo = Game.getWorld().getBlock((int) (posx - 30) / 60, (int) (posy - 29) / 60);
-                Block lu = Game.getWorld().getBlock((int) (posx - 30) / 60, (int) (posy + 29) / 60);
-                if (lo == Block.blocks[4] || lo == null && lu == Block.blocks[4] || lu == null) {
-                    this.move(-1 * geschw, 0);
-                }
-            }
-            if (KeyInput.right && posx+30 < Game.getWorld().getWidth()*60) {
-                Block ro = Game.getWorld().getBlock((int) (posx + 30) / 60, (int) (posy - 29) / 60);
-                Block ru = Game.getWorld().getBlock((int) (posx + 30) / 60, (int) (posy + 29) / 60);
-                if (ro == Block.blocks[4] || ro == null && ru == Block.blocks[4] || ru == null) {
-                    this.move(geschw, 0);
-                }
-            }
-
-        //TESTCODE!! boolean t auch entfernen!
-        /*
-            Für jede Bewegungsrichtung individuell Ecken + 1(in Bewegungsrichtung) überprüfen
-            Unterschied Ecke - Blockposition => Unterschied noch bewegen
-        */
-
-        Block testlo = null;
-        Block testro = null;
-        Block testlu = null;
-        Block testru = null;
-        if (posx+30 < Game.getWorld().getWidth()*60 && posx-30 >= 0 && posy+30 < Game.getWorld().getHeight()*60 && posy-30 >= 0)
-        {
-            testlo = Game.getWorld().getBlock((int) (posx - 30) / 60, (int) (posy - 27) / 60);
-            testro = Game.getWorld().getBlock((int) (posx + 30) / 60, (int) (posy - 30) / 60);
-            testlu = Game.getWorld().getBlock((int) (posx - 30) / 60, (int) (posy + 30) / 60);
-            testru = Game.getWorld().getBlock((int) (posx + 30) / 60, (int) (posy + 30) / 60);
         }
-
-        o = (testlo == Block.blocks[4] || testlo == null) && (testro == Block.blocks[4] || testro == null);
-        u = (testlu == Block.blocks[4] || testlu == null) && (testru == Block.blocks[4] || testru == null);
-        l = (testlu == Block.blocks[4] || testlu == null) && (testlo == Block.blocks[4] || testlo == null);
-        r = (testru == Block.blocks[4] || testru == null) && (testro == Block.blocks[4] || testro == null);
-
+        if (KeyInput.down && posy+30 < Game.getWorld().getHeight()*60) {
+            Block lu = Game.getWorld().getBlock((int) (posx - (width/2) + 1) / 60, (int) (posy + (heigth/2) + geschw - 1) / 60);
+            Block ru = Game.getWorld().getBlock((int) (posx + (width/2) - 1) / 60, (int) (posy + (heigth/2) + geschw - 1) / 60);
+            if (lu == Block.blocks[4] || lu == null) {
+                if (ru == Block.blocks[4] || ru == null) {
+                    this.move(0, geschw);
+                    colisionU = false;
+                }
+                else {
+                    colisionU = true;
+                }
+            }
+        }
+        if (KeyInput.left && posx-30 >= 0) {
+            Block lo = Game.getWorld().getBlock((int) (posx - (width/2) - geschw + 1) / 60, (int) (posy - (heigth/2) + 2) / 60);
+            Block lu = Game.getWorld().getBlock((int) (posx - (width/2) - geschw + 1) / 60, (int) (posy + (heigth/2) - 2) / 60);
+            if (lo == Block.blocks[4] || lo == null) {
+                if (lu == Block.blocks[4] || lu == null) {
+                    this.move(-1 * geschw, 0);
+                    colisionL = false;
+                }
+                else {
+                    colisionL = true;
+                }
+            }
+        }
+        if (KeyInput.right && posx+30 < Game.getWorld().getWidth()*60) {
+            Block ro = Game.getWorld().getBlock((int) (posx + (width/2) + geschw - 1) / 60, (int) (posy - (heigth/2) + 2) / 60);
+            Block ru = Game.getWorld().getBlock((int) (posx + (width/2) + geschw - 1) / 60, (int) (posy + (heigth/2) - 2) / 60);
+            if (ro == Block.blocks[4] || ro == null) {
+                if (ru == Block.blocks[4] || ru == null) {
+                    this.move(geschw, 0);
+                    colisionR = false;
+                }
+                else  {
+                    colisionR = true;
+                }
+            }
+        }
     }
 
     public double getXPosition()
